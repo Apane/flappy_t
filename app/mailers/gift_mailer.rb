a42b1b1d1b65ab6ac15515380ef9958d91dd9476
Elpizo Choi <fu7iin@gmail.com>
2014-03-06 15:13:56 -0800

    move email body to templates, add confirm email

--- app/mailers/gift_mailer.rb
+++ app/mailers/gift_mailer.rb
@@ -7,17 +7,18 @@ class GiftMailer < ActionMailer::Base
     if @info.to_email.present?
       mail(
         :to => @info.to_email,
-        :subject => "#{@info.from} has bought you a Flappy!",
-        :body => <<-EOS.strip
-Hi #{@info.to},
+        :subject => "#{@info.from} has bought you a Flappy!"
+      )
+    end
+  end
 
-You're getting this email because someone has bought you a Flappy as a gift.  We're currently in funding mode, but look for an order confirmation email from us once the project is backed!
+  def confirm(gift_info)
+    @info = gift_info
 
-If you'd like to get a Flappy for someone else as well, you can preorder at our site - http://flappytoy.herokuapp.com.
-
-Thanks,
-The Flappy Team
-        EOS
+    if @info.from_email.present?
+      mail(
+        :to => @info.from_email,
+        :subject => "Thanks for sending Flappy to #{@info.to}"
       )
     end
   end
