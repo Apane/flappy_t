268aebdcd54489001813f14c470d4d2786b78e9a
Elpizo Choi <fu7iin@gmail.com>
2014-03-09 00:45:32 -0800

    fix access keys for amazon flex pay

--- config/initializers/amazon_flexible_payments.rb
+++ config/initializers/amazon_flexible_payments.rb
@@ -1,3 +1,3 @@
-AmazonFlexPay.access_key = ENV["AKIAJZLGMY3KXLGCIDLQ"]
-AmazonFlexPay.secret_key = ENV["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"]
+AmazonFlexPay.access_key = ENV["AWS_ACCESS_KEY_ID"]
+AmazonFlexPay.secret_key = ENV["AWS_SECRET_ACCESS_KEY"]
 AmazonFlexPay.go_live! if Rails.env.production?
