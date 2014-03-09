bced38eb8608e105898cbcdc7d3090395572d391
Apane <Anthony.panepinto101@gmail.com>
2014-03-08 23:25:39 -0500

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Selfstarter::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
@@ -13,8 +12,6 @@ Selfstarter::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
-  config.action_controller.asset_host = "http://flappytoycom.s3.amazonaws.com"
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
